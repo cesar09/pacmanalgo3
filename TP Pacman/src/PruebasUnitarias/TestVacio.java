c1e3e1b690dd549c60e209ae4aae2121bace0c4a
voyalrato <voyalrato@e0491f4a-d606-11de-a7eb-d1396c3fe4e6>
2009-11-23 19:01:03 +0000

    Update de TestVacio

--- TP Pacman/src/PruebasUnitarias/TestVacio.java
+++ TP Pacman/src/PruebasUnitarias/TestVacio.java
@@ -1,11 +1,21 @@
 package PruebasUnitarias;
 import junit.framework.TestCase;
-import TP3.Vacio;
+import TP3.*;
 public class TestVacio extends TestCase {
 	
+	private Nivel unNivel;
+
 	public void setUp(){
+		this.unNivel= new Nivel(null);		
+	}
+	
+	public void testHayPacman(Nivel unNivel) {
+		Laberinto unLaberinto= unNivel.getMiLaberinto();
+		Posicion unaPosicion= unLaberinto.devolverPosicion(1,1);
+		Transitable unContenido= (Vacio)unaPosicion.getContenido();
+		unContenido.hayPacman(unNivel);
 		
-		
+		//TODO: TERMINAR ESTA GAROMPA.
 	}
 
 }
