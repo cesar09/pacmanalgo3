e60f2b3d615b3184bb79c3d7dbe6f50fbbd7fc4f
voyalrato <voyalrato@e0491f4a-d606-11de-a7eb-d1396c3fe4e6>
2009-11-25 23:18:06 +0000

    nuevamente modificacion de los tests

---
+++ trunk/TP Pacman/src/PruebasUnitarias/testPoint.java
@@ -0,0 +1,25 @@
+package PruebasUnitarias;
+
+import junit.framework.TestCase;
+import TP3.*;
+
+public class testPoint extends TestCase{
+
+	private Point punto1;
+	private Point punto2;
+	
+	public void setUp(){
+		this.punto1= new Point();
+		this.punto2=new Point(1,1);		
+	}
+	
+	public void testPointUno(){
+		assertEquals(punto1.getX(),0);
+		assertEquals(punto1.getY(),0);
+	}
+	public void testPointDos(){
+		assertEquals(punto2.getX(),1);
+		assertEquals(punto2.getY(),1);
+	}
+	
+}
