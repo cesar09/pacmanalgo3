1dac1eaa2b383eeee3c3e64e925cbab4329fe252
voyalrato <voyalrato@e0491f4a-d606-11de-a7eb-d1396c3fe4e6>
2009-11-26 16:20:28 +0000

    corrección de fantasmas para q al ir para arriba vallan para arriba jajaj

--- TP Pacman/src/PruebasUnitarias/testBlinky.java
+++ TP Pacman/src/PruebasUnitarias/testBlinky.java
@@ -5,8 +5,40 @@ import TP3.*;
 public class testBlinky extends TestCase{
 
 	private Blinky unFantasma;
+	private Nivel unNivel;
 	
 	public void setUp(){
-		unFantasma=new Blinky(10);		
+		this.unFantasma=new Blinky(10);	
+		Juego unJuego=new Juego();
+		this.unNivel =new Nivel(unJuego);
+	}
+	public void testMoverIzquierda(){
+		Point unPoint=this.unFantasma.obtenerPosicion();
+		assertEquals(unPoint.getX(),13);
+		assertEquals(unPoint.getY(),13);
+		try {
+			this.unFantasma.elegirMovimiento(unNivel);
+		} catch (PacmanSinVidaException e) {
+			fail("No debió lanzar excepción aquí, ya que a esta altura los fantasmas no han salido de la jaula.");
+		}
+		unPoint=this.unFantasma.obtenerPosicion();
+		assertEquals(unPoint.getX(),12);
+		assertEquals(unPoint.getY(),13);
+		try {
+			this.unFantasma.elegirMovimiento(unNivel);
+			this.unFantasma.elegirMovimiento(unNivel);
+		} catch (PacmanSinVidaException e) {
+			fail("No debió lanzar excepción aquí, ya que a esta altura los fantasmas no han salido de la jaula.");
+		}
+		unPoint=this.unFantasma.obtenerPosicion();
+		assertEquals(unPoint.getX(),11);
+		assertEquals(unPoint.getY(),14);
+		try {
+			this.unFantasma.elegirMovimiento(unNivel);
+			this.unFantasma.elegirMovimiento(unNivel);
+			this.unFantasma.elegirMovimiento(unNivel);
+		} catch (PacmanSinVidaException e) {
+			fail("No debió lanzar excepción aquí, ya que a esta altura los fantasmas no han salido de la jaula.");
+		}
 	}
 }
