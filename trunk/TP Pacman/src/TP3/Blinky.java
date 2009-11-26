719e3bf01f4ee917f9871ce29256714dc3c6b456
diegomarquez86 <diegomarquez86@e0491f4a-d606-11de-a7eb-d1396c3fe4e6>
2009-11-26 03:54:59 +0000

    agregue los 4 posibles movimientos de los fantasmas, y ree-implemente los 4 huir y atrapar

--- trunk/TP Pacman/src/TP3/Blinky.java
+++ trunk/TP Pacman/src/TP3/Blinky.java
@@ -6,21 +6,8 @@ public class Blinky extends Fantasma {
 	}
 
 	public void atraparPacman(Nivel unNivel) throws PacmanSinVidaException{
-        Laberinto unLaberinto = unNivel.obtenerMiLaberinto();
         Pacman unPacman = unNivel.obtenerPacman();
-        int x = this.obtenerPosicion().getX();
-        int y = this.obtenerPosicion().getY();
-        try {
-           unLaberinto.devolverContenido(x-1,y).serTransitado(unNivel);
-           this.mover(x-1, y);
-           x--;
-        }catch (NoTransitableException e) {
-            try {
-              unLaberinto.devolverContenido(x,y+1).serTransitado(unNivel);
-              this.mover(x,y+1);
-              y++;
-            }catch (NoTransitableException e2) {}
-            }
+        this.moverIzquierda(unNivel);
         if(unNivel.mismaPosicion(this, unPacman)){
         	unPacman.morir();
         	unNivel.renacerFantasmas();
@@ -29,21 +16,9 @@ public class Blinky extends Fantasma {
 	}
 	
 	public void huirDePacman(Nivel unNivel) {
-        int x = this.obtenerPosicion().getX();
-        int y = this.obtenerPosicion().getY();
-        Laberinto unLaberinto = unNivel.obtenerMiLaberinto();
-        try {
-        		unLaberinto.devolverContenido(x,y+1).serTransitado(unNivel);
-                this.mover(x, y+1);
-                y++;
-        }catch (NoTransitableException e) {
-            	try {
-            		unLaberinto.devolverContenido(x+1,y).serTransitado(unNivel);
-            		this.mover(x+1, y);
-            		x++;
-            	}catch (NoTransitableException e2) {}
-            }
-        if(unNivel.mismaPosicion(this, unNivel.obtenerPacman())){
+		Pacman unPacman = unNivel.obtenerPacman();
+        this.moverDerecha(unNivel);
+        if(unNivel.mismaPosicion(this,unPacman)){
         	this.fantasmaComido(unNivel);
         }
     }
