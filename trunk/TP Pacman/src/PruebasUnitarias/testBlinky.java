9ef9bcdeb07ce5a9815df8c6b9ac189f31b6fa5c
voyalrato <voyalrato@e0491f4a-d606-11de-a7eb-d1396c3fe4e6>
2009-11-26 06:09:16 +0000

    update testpuntopoder

---
+++ trunk/TP Pacman/src/PruebasUnitarias/testBlinky.java
@@ -0,0 +1,12 @@
+package PruebasUnitarias;
+
+import junit.framework.TestCase;
+import TP3.*;
+public class testBlinky extends TestCase{
+
+	private Blinky unFantasma;
+	
+	public void setUp(){
+		unfantasma=new Blinky(10);		
+	}
+}
