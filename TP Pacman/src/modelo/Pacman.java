b2c05bee24df5a4a63d99eec6add52d4c3a6261c
voyalrato <voyalrato@e0491f4a-d606-11de-a7eb-d1396c3fe4e6>
2009-12-15 15:42:44 +0000

    moví el segunSentido para mover el pacman, lo saque de nivel y lo puse en el propio Pacman, arreglé algunos detalles tmb necesarios para que funcione

--- TP Pacman/src/modelo/Pacman.java
+++ TP Pacman/src/modelo/Pacman.java
@@ -11,31 +11,111 @@ public class Pacman extends Personaje implements Posicionable,Movible {
 	private int sentidoX=-1;
 	private int sentidoGraficoY=0;
 	private int sentidoGraficoX=-1;
+	private int ultimoSentidoPacmanX;
+	private int ultimoSentidoPacmanY;
 
 	public Pacman() {
 		this.renacer();
 	}
 	
+	
+	
+	
+	public boolean moverSegunSentido(boolean moverSegunUltimoSentido, Nivel unNivel){
+		int x;
+		int y;
+		int sentidoEnX=this.getSentidoX();
+		int sentidoEnY=this.getSentidoY();
+		if(moverSegunUltimoSentido){	
+			sentidoEnX=ultimoSentidoPacmanX;
+			sentidoEnY=ultimoSentidoPacmanY;
+		}
+		x = this.getX();
+		y = this.getY();
+		
+		switch (sentidoEnX){		
+		case -1:
+			//Si se desea ir para la izquierda.
+			x = x-1;
+			try {
+				 unNivel.obtenerMiLaberinto().devolverContenido(x,y).hayPacman(unNivel,x,y);
+				 this.irIzquierda();
+				 ultimoSentidoPacmanX=-1;
+				 ultimoSentidoPacmanY=0;
+				 if (!unNivel.obtenerMiJuego().seGanoJuego()){
+					unNivel.compararPosicionesConFantasmas();
+					return true;
+				 }
+			} catch (NoTransitableException e) {}
+			 break;
+		case 1:
+			//Si se desea ir para la derecha.
+			x = x+1;
+			  try {
+				 unNivel.obtenerMiLaberinto().devolverContenido(x,y).hayPacman(unNivel,x,y);
+				 this.irDerecha();
+				 ultimoSentidoPacmanX=1;
+				 ultimoSentidoPacmanY=0;
+				 if (!unNivel.obtenerMiJuego().seGanoJuego()){
+					 unNivel.compararPosicionesConFantasmas();
+					 return true;
+				 }
+			  } catch (NoTransitableException e) {}
+			 break;
+		}
+		switch (sentidoEnY){
+		case 1:
+			//Si se desea ir para abajo.
+			y = y+1;
+			try {
+				 unNivel.obtenerMiLaberinto().devolverContenido(x,y).hayPacman(unNivel,x,y);
+				 this.irAbajo();
+				 ultimoSentidoPacmanY=1;
+				 ultimoSentidoPacmanX=0;
+				 if (!unNivel.obtenerMiJuego().seGanoJuego()){
+					 unNivel.compararPosicionesConFantasmas();
+					 return true;
+				 }
+			} catch (NoTransitableException e) {}
+			 break;
+		
+		case -1:
+			y = y-1;
+			try {
+				 unNivel.obtenerMiLaberinto().devolverContenido(x,y).hayPacman(unNivel,x,y);
+				 this.irArriba();
+				 ultimoSentidoPacmanY=-1;
+				 ultimoSentidoPacmanX=0;
+				 if (!unNivel.obtenerMiJuego().seGanoJuego()){
+					 unNivel.compararPosicionesConFantasmas();
+					 return true;
+				 }
+			} catch (NoTransitableException e) {}
+			 break;
+		}
+		return false;
+	}
+		
 	protected void mover(int x, int y) {
 		this.posicion.setXY(x, y);
 	}
 	
-	public void irIzquierda() {
+	protected void irIzquierda() {
 		this.mover((this.posicion.getX()-1),this.posicion.getY());
 		this.nuevoSentidoGrafico(-1,0);
 	}
 	
-	public void irDerecha() {
+	protected void irDerecha() {
 		this.mover((this.posicion.getX()+1),this.posicion.getY());
 		this.nuevoSentidoGrafico(1,0);
 	}
 	
-	public void irArriba() {
+	protected void irArriba() {
 		this.mover(this.posicion.getX(),(this.posicion.getY()-1));
 		this.nuevoSentidoGrafico(0,-1);
 	}
 	
-	public void irAbajo() {
+	protected void irAbajo() {
 		this.mover(this.posicion.getX(),(this.posicion.getY()+1));
 		this.nuevoSentidoGrafico(0,1);
 	}
@@ -93,7 +173,7 @@ public class Pacman extends Personaje implements Posicionable,Movible {
 	public int getSentidoGraficoY() {
 		return sentidoGraficoY;
 	}
-
+	//TODO con esto que onda?
 	public void serTransportadoADerecha() {
 		/*int x = this.obtenerPosicion().getX();
 		int y = this.obtenerPosicion().getY();		
