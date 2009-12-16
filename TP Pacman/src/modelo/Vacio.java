55c71d0a28f4155d1ca6e362fa712f753c3ff314
lea_07_87 <lea_07_87@hotmail.com>
2009-12-16 23:01:36 +0000

    * SAQUE EL TIMERVELOCIDAD Q YA NO USAMOS

--- TP Pacman/src/modelo/Vacio.java
+++ TP Pacman/src/modelo/Vacio.java
@@ -5,7 +5,7 @@ public class Vacio implements Contenido {
 	public Vacio(){
 		
 	}
-	//Queda así el método para poder ser un Contenido sin ser una clase abstracta
+	//El método queda así para poder ser un Contenido sin ser una clase abstracta
 	public void hayPacman(Nivel unNivel,int x,int y) {
 				
 	}
