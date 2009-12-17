b3d2aaced4867fedf027fec06aed9aa475efdc9f
lea_07_87@hotmail.com <lea_07_87@hotmail.com@e0491f4a-d606-11de-a7eb-d1396c3fe4e6>
2009-12-17 21:03:26 +0000

    * MODIFICACION EN UN COMENTARIO

--- trunk/TP Pacman/src/modelo/Contenido.java
+++ trunk/TP Pacman/src/modelo/Contenido.java
@@ -6,10 +6,10 @@ import org.w3c.dom.Element;
 
 public abstract class Contenido {
 		
-	//Este método se usa cuando el pacman está parado en el casillero del bloque
+	//Este método se usa cuando el pacman está parado en el casillero del contenido en cuestión.
 	abstract public void hayPacman(Nivel unNivel,int x,int y) throws NoTransitableException;
 	
-	//Este método se usa cuando un fantasma está parado en el casillero del bloque
+	//Este método se usa cuando un fantasma está parado en el casillero del contenido en cuestión.
 	abstract public void serTransitado(Nivel unNivel, Fantasma unFantasma, int x, int y) throws NoTransitableException;
 
 	abstract public Element guardar(Document doc, int x, int y);
