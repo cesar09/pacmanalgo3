4a6ce30ab703a189e2789aa716147d5db5bf5311
lea_07_87 <lea_07_87@hotmail.com>
2009-12-17 18:52:19 +0000

    * ALGUNAS PRUEBAS MAS

--- TP Pacman/src/pruebas/testJuego.java
+++ TP Pacman/src/pruebas/testJuego.java
@@ -2,7 +2,7 @@ package pruebas;
 
 
 import modelo.Juego;
-
+import modelo.JuegoGanadoException;
 import junit.framework.TestCase;
 
 public class testJuego extends TestCase{
@@ -22,4 +22,32 @@ public class testJuego extends TestCase{
 		assertTrue(this.unJuego.seGanoNivel());
 	}
 	
+	public void testGanarJuego(){
+		boolean juegoGanado =false;
+		try{
+			this.unJuego.pasarDeNivel();
+			this.unJuego.pasarDeNivel();
+			this.unJuego.pasarDeNivel();
+			this.unJuego.pasarDeNivel();
+		}catch (JuegoGanadoException e){
+			fail("No debía arrojar la excepción aún.");
+		}
+		try{
+			this.unJuego.pasarDeNivel();
+		}catch (JuegoGanadoException e){
+			juegoGanado = true;
+		}
+		if (juegoGanado){
+			assertTrue(true);
+		}else{
+			assertTrue(false);
+		}
+	}
+	
+	public void testSumarPuntajeYSumarVida(){
+		this.unJuego.sumarPuntajeAlJugador(10000);
+		assertEquals(this.unJuego.obtenerPuntajeDelJugador(),10000);
+		assertEquals(this.unJuego.obtenerVidasDisponibles(),4);
+	}
+			
 }
