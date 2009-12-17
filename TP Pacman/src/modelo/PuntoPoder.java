99274fd00442d449fcec8bcbce14be223fd76128
lea_07_87 <lea_07_87@hotmail.com>
2009-12-17 05:43:21 +0000

    * AGREGADO TODO LO DE PERSISTENCIA

--- TP Pacman/src/modelo/PuntoPoder.java
+++ TP Pacman/src/modelo/PuntoPoder.java
@@ -1,9 +1,17 @@
 package modelo;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 public class PuntoPoder extends Comestible {
 
 	public PuntoPoder (){
 		this.puntaje = 50;
+	}
+	
+	//Para el constructor por persistencia
+	public PuntoPoder(int puntaje) {
+		this.puntaje=puntaje;
 	}
 	
 	public void hayPacman(Nivel unNivel,int x,int y) {
@@ -27,6 +35,19 @@ public class PuntoPoder extends Comestible {
 		}else{
 			new TimerComestible (2, unNivel);
 		}
+	}
+	
+	public Element guardar(Document doc, int x, int y) {
+		Element elemPuntoPoder = doc.createElement("ContenidoColumna"+x+"Fila"+y);
+		elemPuntoPoder.setAttribute("puntaje",""+this.puntaje);
+		elemPuntoPoder.setAttribute("tipo",""+"PuntoPoder");
+		return elemPuntoPoder;
+	}	
+
+	public static Contenido recuperar(Element elemContenido) {
+		int puntaje= Integer.parseInt(elemContenido.getAttribute("puntaje"));
+		Contenido unPuntoPoder = new PuntoPoder(puntaje);
+		return unPuntoPoder;
 	}
 	
 }
